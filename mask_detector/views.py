a9156a805f7eeb55d4c153792815e7a97f07fb51
venky <venkyeswar2037@gmail.com>
2024-10-06 01:18:47 +0530

    updated the model accuracy

--- mask_detector/views.py
+++ mask_detector/views.py
@@ -5,6 +5,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import numpy as np
 import cv2
+from PIL import Image
 import os
 from keras.models import load_model
 from django.conf import settings
@@ -36,21 +37,19 @@ def result(request, pk):
     return render(request, 'mask_detector/result.html', {'mask_image': mask_image})
 
 def predict_mask(image):
-    file_bytes = np.asarray(bytearray(image.read()),dtype=np.uint8)
-    img = cv2.imdecode(file_bytes,cv2.IMREAD_COLOR)
+    image = Image.open(image)
+    image = image.resize((128, 128))  # Resize the image for the model
+    image = img_to_array(image)       # Convert image to array
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    image = image / 255.0  # Normalize the image
 
-    if img is None:
-        raise ValueError("Image not Loaded Correctly")
-    
-    img_resized = cv2.resize(img,(128,128))
-    img_array = np.array(img_resized)/255.0
-    img_array = np.expand_dims(img_array,axis=0)
-      
-    prediction = model.predict(img_array)[0][0]
+    # Predict using the loaded model
+    prediction = model.predict(image)[0][0]
 
     # Determine the label and confidence
-    if prediction > 0.8:
+    if prediction < 0.9:
         return "Without Mask", float(prediction)
     else:
         return "With Mask", float(prediction)
+
 
